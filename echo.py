55b1e9afb161abf0ee6a4322aee103070be77c20
zhangjunqing <zhangjunqing0226@163.com>
2025-10-20 15:48:17 +0800

    docs: add usage notes

--- echo.py
+++ echo.py
@@ -3,6 +3,10 @@ import sys
 def echo():
     """
     获取用户输入并打印出来，支持-s标志以大写形式输出
+    
+    使用方法:
+    - python echo.py (普通模式)
+    - python echo.py -s (大写模式)
     """
     shout = "-s" in sys.argv
     message = input("Please enter something: ")
