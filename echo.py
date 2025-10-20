e3abccbe3ead9be47c290370f7d346f5fc630753
zhangjunqing <zhangjunqing0226@163.com>
2025-10-20 15:39:11 +0800

    feat: support -s to shout

--- echo.py
+++ echo.py
@@ -1,9 +1,12 @@
+import sys
+
 def echo():
     """
-    获取用户输入并打印出来
+    获取用户输入并打印出来，支持-s标志以大写形式输出
     """
+    shout = "-s" in sys.argv
     message = input("Enter something: ")
-    print(message)
+    print(message.upper() if shout else message)
 
 if __name__ == "__main__":
     echo()
